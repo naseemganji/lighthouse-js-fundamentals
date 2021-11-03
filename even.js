3454201db8ab28e97d6fd804f69c0d36c7b208d4
Naseem Ganji <91971476+naseemganji@users.noreply.github.com>
2021-11-03 09:06:46 -0400

    Update even.js

--- even.js
+++ even.js
@@ -1,9 +1,4 @@
 function isEven(num) {
-  return num % 2 === 0;
-}
-
-// var tenIsEven = isEven(10);
-// var elevenIsEven = isEven(11);
-
+  return num % 2 === 0;}
 console.log(isEven(10));
 console.log(isEven(11));
