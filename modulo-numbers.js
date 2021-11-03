268bce0f699c300a358526c86269d0c0470c8676
Naseem Ganji <91971476+naseemganji@users.noreply.github.com>
2021-11-03 09:09:15 -0400

    Update modulo-numbers.js

--- modulo-numbers.js
+++ modulo-numbers.js
@@ -3,11 +3,9 @@ let hits = 0;
 let startingPoint = 0;
 let currentPoint = startingPoint;
 let endingPoint = 1000;
-
 for ( ; currentPoint <= endingPoint; currentPoint++) {
   if (currentPoint % divisor == 0) {
     console.log(currentPoint);
-    hits += 1; // What's the difference between += and ++ when trying to increment a variable?
+    hits += 1; 
   }
-}
-  console.log("There were " + hits + " numbers between " + startingPoint + " and " + endingPoint + " that are evenly divisible by " + divisor + ".");
+}console.log("There were " + hits + " numbers between " + startingPoint + " and " + endingPoint + " that are evenly divisible by " + divisor + ".");
