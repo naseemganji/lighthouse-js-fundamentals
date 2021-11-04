65c167bac70cda9934f057b850de837eff58b31f
Naseem Ganji <naseem.ganji@icloud.com>
2021-11-04 14:23:52 -0400

    urlencoding

--- urlencodeing.js
+++ urlencodeing.js
@@ -1,3 +1,29 @@
-var str = "A quick Brown Fox";   
-var replaced = str.replace(" ", "%20");
-console.log(replaced)
+const urlEncode = (string)=>{
+  //remove white space at the end 
+  const arrString = string.split("")
+  for(let i = arrString.length -1 ; i>=0 ; i--){
+    let char = arrString[i];
+    
+    if(char.indexOf(" ") >=0){
+     arrString.splice(i,1)
+    }else{
+      break;
+    }
+  }
+
+  let start =0;
+  let end = arrString.length -1;
+  
+
+  //add %20
+  while(start < end){
+    if(arrString[start].indexOf(' ') >=0){
+      arrString[start] ="%20"
+      
+    }
+    
+    start++;
+  }
+  
+  return arrString.join('');
+}
